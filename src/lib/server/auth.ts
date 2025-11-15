1189296978626a15e5fead395a3d2e8ec7508350
Kevin Wade <kevinrwade@gmail.com>
2025-11-15 13:16:24 -0600

    Add docker compose, prisma, better-auth

---
+++ src/lib/server/auth.ts
@@ -0,0 +1,20 @@
+import { betterAuth } from 'better-auth'
+import { prismaAdapter } from 'better-auth/adapters/prisma'
+import { db } from './db'
+
+export const auth = betterAuth({
+  database: prismaAdapter(db, {
+    provider: 'postgresql',
+  }),
+  emailAndPassword: {
+    enabled: false,
+  },
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
+  },
+  secret: process.env.BETTER_AUTH_SECRET as string,
+  baseURL: process.env.BETTER_AUTH_URL as string,
+})
