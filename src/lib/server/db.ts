b5eacdb6d2ebd9fa8adbccb310d9dcc7e59dd9a0
Kevin Wade <kevinrwade@gmail.com>
2025-11-23 15:06:28 -0600

    Upgrade to prisma v7

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -1,11 +1,17 @@
 import { PrismaClient } from '@/generated/prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import pg from 'pg';
 
 export type {
   Debt as DebtEntity,
   Workbook as WorkbookEntity,
 } from '@/generated/prisma/client';
 
+const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL });
+const adapter = new PrismaPg(pool);
+
 export const db = new PrismaClient({
+  adapter,
   log:
     process.env.NODE_ENV === 'development'
       ? ['query', 'info', 'warn', 'error']
